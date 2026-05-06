18462e40aae0e8026e2c95f2f95700388c9be603
Rodrigo <rodrigoetomas22@gmail.com>
2026-05-06 11:40:19 +0100

    criação da class product

---
+++ projetofinal/lib/models/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  final String name;
+  final String id;
+  final double price;
+  final double quantity;
+
+  Product({required this.name, required this.id, required this.price, required this.quantity}
+  );
+}
